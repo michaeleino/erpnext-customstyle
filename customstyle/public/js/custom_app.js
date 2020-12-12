0b80348a972f81d69670a0aaa778dc801dd8aeef
michaeleino <michaeleino@hotmail.com>
2020-12-13 00:08:31 +0200

    test by python

--- customstyle/public/js/custom_app.js
+++ customstyle/public/js/custom_app.js
@@ -3,7 +3,7 @@
 //     // frappe.set_route("frappe.ui.toolbar.setup_session_defaults();");
 //     frappe.ui.toolbar.setup_session_defaults();
 // });
-$(document).on("login", function () {
+$(document).on("notify_on_every_login", function () {
     // custom logic
     // frappe.set_route("frappe.ui.toolbar.setup_session_defaults();");
     frappe.ui.toolbar.setup_session_defaults();
