84b64f012032581944b5d4851db8b6618aef754c
michaeleino <michaeleino@hotmail.com>
2020-12-13 01:09:31 +0200

    try python

--- customstyle/overrides.py
+++ customstyle/overrides.py
@@ -1,3 +1,4 @@
-# def setup_session_defaults(login_manager):
-#     # allocate free credits to frappe.session.user
-#     frappe.ui.toolbar.setup_session_defaults();
+def start_session_defaults():
+    # allocate free credits to frappe.session.user
+    # frappe.ui.toolbar.setup_session_defaults();
+    console.log(frappe.ui.toolbar.setup_session_defaults());
