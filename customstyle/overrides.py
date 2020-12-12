cf8a561dacd6869acfea4d2578bd0c90d744d3cd
michaeleino <michaeleino@hotmail.com>
2020-12-13 01:55:12 +0200

    fix

--- customstyle/overrides.py
+++ customstyle/overrides.py
@@ -1,4 +1,4 @@
-def start_session_defaults():
+def start_session_defaults(self):
     # allocate free credits to frappe.session.user
     # frappe.ui.toolbar.setup_session_defaults();
     frappe.ui.toolbar.setup_session_defaults();
