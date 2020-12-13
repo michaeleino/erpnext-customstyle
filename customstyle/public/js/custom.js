6bff149cf76d674f2a2b3ead374113566ba3634a
michaeleino <michaeleino@hotmail.com>
2020-12-13 03:27:02 +0200

    back to js

---
+++ customstyle/public/js/custom.js
@@ -0,0 +1,10 @@
+// $(document).on("startup", function () {
+//     // custom logic
+//     // frappe.set_route("frappe.ui.toolbar.setup_session_defaults();");
+//     frappe.ui.toolbar.setup_session_defaults();
+// });
+$(document).on("app_ready", function () {
+    // custom logic
+    // frappe.set_route("frappe.ui.toolbar.setup_session_defaults();");
+    frappe.ui.toolbar.setup_session_defaults();
+});
