2227c76c213da20523c9eba4e9603ec9efd52ef4
michaeleino <michaeleino@hotmail.com>
2021-02-14 14:37:47 +0200

    fix/commit

--- customstyle/overrides.py
+++ customstyle/overrides.py
@@ -2,3 +2,6 @@
 #     # allocate free credits to frappe.session.user
 #     # frappe.ui.toolbar.setup_session_defaults();
 #     frappe.ui.toolbar.setup_session_defaults();
+# import frappe
+# def default_company_cookie(self):
+#     if
