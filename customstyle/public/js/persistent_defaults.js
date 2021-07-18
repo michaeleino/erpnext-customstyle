07499d5ea7ed2d50e96729c76a71129f299bca2d
michaeleino <michaeleino@hotmail.com>
2021-07-18 17:53:52 +0200

    fix and add the fallback to to popup the setup_session_defaults

--- customstyle/public/js/persistent_defaults.js
+++ customstyle/public/js/persistent_defaults.js
@@ -1,23 +1,26 @@
 $(document).on('app_ready', function() {
-      if (document.referrer.endsWith("/login")) {
+      if (document.referrer.endsWith("/")) {
           // app ready after login, let's rumble
           console.log("let's rumble");
           // get persistent session settings
           frappe.call({
               'method': "frappe.client.get_list",
               'args': {
-                  'doctype': "Persistent Session Setting",
-                  'filters': [[ "UsersDoc","user","in",frappe.session.user ]],
-                  // {'users': frappe.session.user},
+                  'doctype': "Employee",
+                  'filters': [[ "Employee","user_id","=",frappe.session.user ]],
                   'fields': ["company"]
               },
               'callback': function(response) {
-                  if (response.message) {
+                  if (response.message > 0) {
+                    console.log(response.message.length)
                       response.message.forEach(function (setting) {
-                          //console.log(setting.company);
+                          console.log(setting.company);
                           // var key = setting.Key1.toLowerCase().replaceAll(" ", "_");
                           frappe.defaults.set_user_default_local("company", setting.company);
                       });
+                  }
+                  else{ // fallback to popup the setup_session_defaults
+                      frappe.ui.toolbar.setup_session_defaults();
                   }
               }
           });
