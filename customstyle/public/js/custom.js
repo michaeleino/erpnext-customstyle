bfe12f7250a8cf5d756ae0dfea722766c0fc9a0b
michaeleino <michaeleino@hotmail.com>
2021-07-18 13:06:08 +0200

    disable the auto popup set the defauult session sessting as per https://discuss.erpnext.com/t/feature-request-session-defaults-persist-between-logins/54274/9

--- customstyle/public/js/custom.js
+++ customstyle/public/js/custom.js
@@ -3,8 +3,8 @@
 //     // frappe.set_route("frappe.ui.toolbar.setup_session_defaults();");
 //     frappe.ui.toolbar.setup_session_defaults();
 // });
-$(document).on("app_ready", function () {
-    // custom logic
-    // frappe.set_route("frappe.ui.toolbar.setup_session_defaults();");
-    frappe.ui.toolbar.setup_session_defaults();
-});
+// $(document).on("app_ready", function () {
+//     // custom logic
+//     // frappe.set_route("frappe.ui.toolbar.setup_session_defaults();");
+//     frappe.ui.toolbar.setup_session_defaults();
+// });
---
+++ customstyle/public/js/persistent_defaults.js
@@ -0,0 +1,23 @@
+$(document).on('app_ready', function() {
+      if (document.referrer.endsWith("/login")) {
+          // app ready after login, let's rumble
+          console.log("let's rumble");
+          // get persistent session settings
+          frappe.call({
+              'method': "frappe.client.get_list",
+              'args': {
+                  'doctype': "Persistent Session Setting",
+                  'filters': {'user': frappe.session.user},
+                  'fields': ["key1", "value1"]
+              },
+              'callback': function(response) {
+                  if (response.message) {
+                      response.message.forEach(function (setting) {
+                          var key = setting.Key1.toLowerCase().replaceAll(" ", "_");
+                          frappe.defaults.set_user_default_local(key, setting.value1);
+                      });
+                  }
+              }
+          });
+      }
+  });
