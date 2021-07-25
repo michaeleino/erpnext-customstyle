21754f7f0501e78ee24935b94a59355af804ad7b
michaeleino <michaeleino@hotmail.com>
2021-07-25 16:28:08 +0200

    prettify & add comments

--- customstyle/public/js/clear_persistent_defaults.js
+++ customstyle/public/js/clear_persistent_defaults.js
@@ -1,10 +1,5 @@
-  // $(document).on('app_ready', function() {
-    // $(document).on('startup', function() {
-    //  let My_session_default_settings = getCookie("My_session_default_settings");
-    if (document.cookie.split(';').some((item) => item.includes('my_session_default_settings=yes' && 'system_user=no' && 'user_id=Guest'))) {
-      document.cookie = "my_session_default_settings=no;SameSite=Lax";
-      console.log("my_session_default_settings=no");
-
-    }
-
-  // });
+//check if logged out and set the my_session_default_settings=no
+if (document.cookie.split(';').some((item) => item.includes('my_session_default_settings=yes' && 'system_user=no' && 'user_id=Guest'))) {
+  document.cookie = "my_session_default_settings=no;SameSite=Lax";
+  console.log("my_session_default_settings=no");
+}
